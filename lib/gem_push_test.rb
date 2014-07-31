260f1b2b33079cd1fbcb4fe6ea6761f16892c21f
pasuya1981 <pasuya1234@hotmail.com>
2014-07-31 14:38:36 +0800

    initial commit

---
+++ lib/gem_push_test.rb
@@ -0,0 +1,7 @@
+require "gem_push_test/version"
+
+module GemPushTest
+  def self.say_something(string)
+    puts "Hi, I wanna say: #{ string } "
+  end
+end
---
+++ lib/gem_push_test/version.rb
@@ -0,0 +1,3 @@
+module GemPushTest
+  VERSION = "1.0.0"
+end
